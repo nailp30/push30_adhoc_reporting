6e6bbba0d54023770417651c743f21fee1fca204
Nail Mustafazade <nail.mustafazade@push30.az>
2025-12-22 14:57:25 +0400

    Update black_friday_2025.sql

--- black_friday/black_friday_2025.sql
+++ black_friday/black_friday_2025.sql
@@ -41,7 +41,20 @@ and r.promo_code = 'PROMO50'
 
 prev_subs as (
 select 
-rr.user_id, max(rr.subscribed_at) as previous_subscription
+rr.user_id, 
+max(
+case when rr.canceled_at is not null then rr.canceled_at
+else (case when rr.periods = 'Monthly' then rr.subscribed_at + interval '1 Month'
+		   when rr.periods = 'Annual'  then rr.subscribed_at + interval '1 Year' 
+		   end
+		   ) 
+end) as last_subscription_end 
+
+
+
+
+
+
 from aze.dm_subscription_report rr 
 inner join black_friday bf on rr.user_id = bf.user_id
 and rr.subscribed_at < bf.subscribed_at
@@ -53,12 +66,11 @@ group by rr.user_id
 )
 
 select bf.*, 
-ps.previous_subscription,
-case when ps.previous_subscription is null then 'New user'
-	 when extract(DAY FROM (bf.subscribed_at - ps.previous_subscription)) >= 72 then 'Churn back'
-	 when extract(DAY FROM (bf.subscribed_at - ps.previous_subscription)) < 72 then 'Renewal'
+ps.last_subscription_end,
+case when ps.last_subscription_end is null then 'New user'
+	 when extract(DAY FROM (bf.subscribed_at - ps.last_subscription_end)) >= 72 then 'Churn back'
+	 when extract(DAY FROM (bf.subscribed_at - ps.last_subscription_end)) < 72 then 'Renewal'
 	 end as customer_type 
 from black_friday bf
 left join prev_subs ps on bf.user_id =ps.user_id
-
-
+order by bf.subscribed_at asc
